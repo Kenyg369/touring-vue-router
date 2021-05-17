1706dd39ac9ccdedcea6f420ba1a7f4b143d35f2
Kenyg369 <kengrand369@gmail.com>
2021-05-17 14:49:42 -0500

    L5-s

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Events</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </div>
   <router-view />
 </template>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="event-card">
+    <p>@{{event.time}} on @{{event.data}}, 2022</p>
+    <h3>{{event.title}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event:{
+      type: Object ,
+      required: true
+    }
+
+  }
+};
+</script>
+
+<style scoped>
+.event-card{
+  width: 250px;
+  padding: 20px;
+  border: 1px solid rgb(58, 57, 57);
+  cursor: pointer;
+  margin-bottom: 25px;
+}
+.event-card:hover{
+  transform: scale(1.05) ;
+  box-shadow: 0px 5px 5px 5px rgba(0, 0, 0, 0.2);
+}
+
+</style>
